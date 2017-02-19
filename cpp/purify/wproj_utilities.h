444cc019562c5fa261648007880ae42e3172ae81
Arwa Dabbech <dabbecharoua@gmail.com>
2017-02-19 21:23:06 +0000

    replaced sparse vectors
    memory problems found when filling  sparse vectors.

--- cpp/purify/wproj_utilities.h
+++ cpp/purify/wproj_utilities.h
@@ -16,12 +16,13 @@
 namespace purify {
   namespace wproj_utilities {
     Matrix<t_complex> generate_chirp(const t_real & w_rate, const t_real &cell_x, const t_real & cell_y, const t_int & x_size, const t_int & y_size);
-    Eigen::SparseVector<t_complex> create_chirp_row(const t_real & w_rate, const t_real &cell_x, const t_real & cell_y,const t_real & ftsizev, const t_real & ftsizeu, const t_real& energy_fraction);
+    Sparse<t_complex> create_chirp_row(const t_real & w_rate, const t_real &cell_x, const t_real & cell_y,const t_real & ftsizev, const t_real & ftsizeu, const t_real& energy_fraction);
     
-    void sparsify_row_sparse(Eigen::SparseVector<t_real> &row,  const t_real &energy);
+    void sparsify_row_sparse(Sparse<t_real> &row, const t_real &energy);
     void sparsify_row_sparse_dense(Eigen::SparseVector<t_real> &row, const t_real &abs_row_max, const t_real &energy);
-
-    Eigen::SparseVector<t_complex> row_wise_convolution( Eigen::SparseVector<t_complex> &Grid,  Eigen::SparseVector<t_complex> &Chirp,  const t_int &Nx,  const t_int &Ny);
+    t_real  sparsify_row_thres(const Sparse<t_real> &row, const t_real &energy);
+    t_real  sparsify_row_dense_thres(const Matrix<t_complex> &row, const t_real &energy);
+    Sparse<t_complex> row_wise_convolution( Eigen::SparseVector<t_complex> &Grid,  Sparse<t_complex> &Chirp,  const t_int &Nx,  const t_int &Ny);
     Sparse<t_complex> wprojection_matrix(const Sparse<t_complex> &Grid, const t_int& Nx, const t_int& Ny,const Vector<t_real> & w_components, const t_real &cell_x, const t_real &cell_y, const t_real& energy_fraction_chirp,const t_real& energy_fraction_wproj);
     
     t_real snr_metric(const Image<t_real> &model, const Image<t_real> &solution);
@@ -30,7 +31,7 @@ namespace purify {
     t_real sparsity_sp(const Sparse<t_complex> & Gmat);
     t_real sparsity_im(const Image<t_complex> & Cmat);
 
-    Eigen::SparseVector<t_complex> generate_vect(const t_int & x_size, const t_int & y_size,const t_real &sigma,const t_real &mean);
+    Sparse<t_complex> generate_vect(const t_int & x_size, const t_int & y_size,const t_real &sigma,const t_real &mean);
     t_real upsample_ratio_sim(const utilities::vis_params& uv_vis, const t_real& L, const t_real& M, const t_int& x_size, const t_int& y_size, const t_int& multipleOf);
 
   }
