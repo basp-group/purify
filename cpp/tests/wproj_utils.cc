eae351587274a5303822c156bcc69a9970757a64
luke <Luke.Pratley.15@ucl.ac.uk>
2017-02-19 22:33:51 +0000

    fixed unit test for w projection, but it still fails

--- cpp/tests/wproj_utils.cc
+++ cpp/tests/wproj_utils.cc
@@ -10,21 +10,26 @@ using namespace purify::notinstalled;
 
 TEST_CASE("wprojection_matrix") {
   //! test if convolution is identity 
-  const auto w_components = Vector<t_real>::Random(12) * 0;
+  const Vector<t_real> w_components = Vector<t_real>::Random(12) * 0;
   const t_int Nx = 128;
   const t_int Ny = 128;
-  const t_int rows = w.size();
+  const t_int rows = w_components.size();
   const t_int cols = Nx * Ny;
 
   Sparse<t_complex> I(rows, cols);
   I.reserve(Vector<t_int>::Constant(rows, 1));
-  for (t_int i = 0; i < std::min(rows, cols) i++) {
+  for (t_int i = 0; i < std::min(rows, cols); i++) {
     I.coeffRef(i, i) = 1;
   }
 
-  const auto G = wproj_utilities::wprojection_matrix(I, Nx, Ny,
+  auto G = wproj_utilities::wprojection_matrix(I, Nx, Ny,
       w_components, 10, 10, 0.9, 0.9);
-  for (t_int i = 0; i < std::min(rows, cols); i++) {
-    CHECK(std::abs(G.coeffRef(i, i) - I.coeffRef(i, i)) < 1e-7);
-  }
+
+  CHECK(G.nonZeros() == I.nonZeros());
+  for (t_int k = 0; k < G.outerSize(); ++k)
+      for (Sparse<t_complex>::InnerIterator it(G, k); it; ++it)
+         {
+             CHECK(it.value() == 1.);
+             CHECK(it.row() == it.col());   // row index
+           }
 }
